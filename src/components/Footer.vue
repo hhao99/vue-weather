a0dde02013c20d2f790df72a2a5843006f5588ba
hhao99 <hhao99@gmail.con>
2025-01-30 12:01:45 -0500

    tailwindv4 ok

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3>&copy;2007-2025; Ruipu Software Co. Ltd.</h3>
+  </div>
+</template>
+<script lang="ts" setup></script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="flex flex-col justify-between">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Banner msg="Weather forecast protect you from bad weather!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+import Banner from '@/components/Banner.vue'
+</script>
